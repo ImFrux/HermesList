dacf985cd5c019e437f205185c617774f563ad8b
WANTY William <ww@WX-OR1031454.ad-subs.w2k.francetelecom.fr>
2023-09-15 15:54:08 +0200

    adding /database endpoint

--- src/database/getLevelDb.js
+++ src/database/getLevelDb.js
@@ -2,5 +2,5 @@ const LevelDB = require("../../src/database/db.js");
 const path = require("path");
 
 module.exports.dbHelper = new LevelDB(
-	path.resolve(process.env.LEVELDB_FOLDER)
+	path.resolve(__dirname, process.env.LEVELDB_FOLDER)
 );
